468e5c37f6ce5851ccd2f6ef904fe0f5ba24a5f7
allard112 <32762757+allard112@users.noreply.github.com>
2023-06-20 14:19:48 +0200

    Add files via upload

---
+++ sketch_jun20a.ino
@@ -0,0 +1,37 @@
+//import libraries
+
+//earthquake
+#define earthquake_japan 1
+#define earthquake_haiti 2
+#define earthquake_nepal 3
+#define earthquake_turkey 4
+#define earthquake_the_netherlands 5
+#define earthquake_usa 6
+#define earthquake_china 7
+#define earthquake_italy 8
+#define earthquake_chile 9
+#define earthquake_egypt 10
+#define earthquake_greece 11
+#define earthquake_guatemala 12
+#define earthquake_myanmar 13
+#define earthquake_russia 14
+#define earthquake_indonesia 15
+#define earthquake_new_zealand 16
+#define earthquake_mexico 17
+
+//variables
+int modeState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switch(modeState) {
+
+  }
+
+
+}
